47da32c23c35bb180d8b9f40f29e07efb7e344d2
Emmanuel Afrifah Ampomah <eaampomah@yahoo.com>
2022-11-30 04:34:54 +0200

    styles added

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">  <!-- Bootsrtap -->
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css" media="screen"> <!-- style directory -->
+     <!--font-family-->
+
+
+    <title>Project CRUD Application</title>
+</head>
+<body>
+<main>
+    <div class="container-lg-fluid container-md container-sm container-xs mt-xs-5 mt-sm-5 mt-sm-5 mt-md-5">
+      <div class="row mainPage">
+        <div class="col col-md-60 col-xs-4">
+          <div class="contents">
+            <h1>List of Clients</h1>
+            <button type="button" class="btn btn-primary">Add new client</button>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>
+</body>
+
+<script src="bootstrap\js\bootstrap.bundle.js"></script> <!-- bootstrap javascript -->
+</html>
