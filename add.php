bd79c058906f53cf9008e69f605d41ee9ac85d97
Emmanuel Afrifah Ampomah <eaampomah@yahoo.com>
2022-12-13 01:42:21 +0000

    form added

---
+++ add.php
@@ -0,0 +1,127 @@
+<?php
+  require_once 'backend/dbconnection.php';
+
+
+$name="";
+$email="";
+$phone="";
+$address="";
+
+// initialize the error message variable
+$errorMessage = "";
+
+// initialize the success message variable
+$successMessage="";
+
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+    $address=$_POST['address'];
+
+    //checking whether there is no empty field. We 'break' from the loop if there's any error and show an error message
+    do {
+        if (empty($name)||empty($email)||empty($phone)||empty($address)) {
+            $errorMessage = 'All fields are required';
+            break;
+        }
+
+            // if there is no empty field then we proceed to create a new client to database
+            $sql = "INSERT INTO clients_data (name, email, phone, address)" . 
+            "VALUES ('$name', '$email', '$phone', '$address')";
+            $result = $connection->query($sql);
+
+        if (!$result) {
+            die("could not add data to database: " . $connection->connect_error);
+            break;
+        }
+
+            // after adding client, we initialize the data variables to empty values
+
+            $name="";
+            $email="";
+            $phone="";
+            $address="";
+
+            // we proceed to show a success message
+            $successMessage="Client successfully added to database";
+
+            // redirects to list of clients page
+            header("location: index.php");
+            exit;
+        }while(false);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adding new client page</title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">  <!-- Bootsrtap -->
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css" media="screen"> <!-- style directory -->
+</head>
+<body class="bg-secondary">
+    <div class="container my-5">
+    <div class="row">
+    <h1>Add new Client</h1>
+        <div class="col-12 my-3">
+            <?php
+            if (!empty($errorMessage)) {
+                echo "
+                <div class='alert alert-warning alert-dismissible fade show'>
+                <strong>$errorMessage</strong>
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>
+                ";
+            };
+            ?>
+            <form method="post">
+             <div class="form-group mb-3">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" placeholder="Full name" name="name" value="<?=$name?>">
+             </div>
+             <div class="form-group mb-3">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" placeholder="@example.com" name="email" value="<?=$email?>">
+             </div>
+             <div class="form-group mb-3">
+               <label for="phone">Phone</label>
+               <input type="text" class="form-control" placeholder="" name="phone" value="<?=$phone?>">
+             </div>
+             <div class="form-group mb-3">
+              <label for="address">Address</label>
+              <input type="text" class="form-control" placeholder="" name="address" value="<?=$address?>">
+             </div>
+             <?php
+             if(!empty($successMessage)) {
+                echo "
+                <div class='row mb-3'>
+                <div class='offset-sm-3 col-sm-6'>
+                <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                <strong>$successMessage</strong>
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>
+                </div>
+                </div>
+                ";
+            }
+             ?>
+            <div class="row mb-3">
+               <div class="offset-sm-3 col-sm-3 d-grid">
+                  <button id="submit" type="submit" class="btn btn-primary mb-1" name="submit">Submit</button>
+               </div>
+               <div class="col-sm-3 d-grid">
+                 <a class="btn btn-danger mb-1" href="" role="cancel">cancel</a>
+               </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+<script src="bootstrap\js\bootstrap.bundle.js"></script> <!-- bootstrap javascript -->
+</html>
--- index.php
+++ index.php
@@ -14,38 +14,38 @@
     <title>Project CRUD Application</title>
 </head>
 <body class="bg-secondary">
-<main>
-
-    <div class="container-lg-fluid container-md container-sm container-xs mt-xs-5 mt-sm-5 mt-sm-5 mt-md-5 my-5">
-      <div class="row mainPage">
-        <div class="col col-md-60 col-xs-4">
-          <div class="contents">
+    <div class="container container-lg-fluid container-md container-sm container-xs mt-xs-5 mt-sm-5 mt-sm-5 mt-md-5 my-5">
+      <div class="row">
+        <!-- <div class="col col-md-60 col-xs-4">
+          <div class="contents"> -->
             <h1 class="text-center text-light center">List of Clients</h1>
-            <button type="button" href="create.php" class="btn btn-primary mb-3">Add new client</button>
-            <table class="table table-warning table-hover table-striped">
-  <thead class="table-danger">
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Address</th>
-      <th scope="col">Created_At</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
+            <div class="col-12 my-3 table-responsive table-responsive-md table-responsive-sm table-responsive-xs">
+            <a href="add.php" class="btn btn-primary mb-3 col-sm-3 d-grid">Add new client</a>
+            <div class="table-responsive table-responsive-lg table-responsive-xl table-responsive-xxl table-responsive-md table-responsive-sm table-responsive-xs">
+            <table class="table table-bordered border-warning table-warning table-hover table-striped">
+            <thead class="table-danger">
+      
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Address</th>
+              <th scope="col">Created_At</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
   <?php
-require_once 'backend/dbcredentials.php';
-require_once 'backend/dbconnection.php';
+require_once 'backend/fetch.php';
 ?>
   </tbody>
 </table>
-          </div>
-        </div>
+            </div>
+          <!-- </div>-->
+        </div> 
       </div>
     </div>
-</main>
 </body>
 
 <script src="bootstrap\js\bootstrap.bundle.js"></script> <!-- bootstrap javascript -->
