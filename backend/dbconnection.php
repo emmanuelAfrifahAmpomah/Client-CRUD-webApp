eb30f20b6d1aa37851d01b78e8de3a6d1ded4fe4
Emmanuel Afrifah Ampomah <eaampomah@yahoo.com>
2022-12-22 22:07:36 +0000

    delete class implemented

--- backend/dbconnection.php
+++ backend/dbconnection.php
@@ -39,6 +39,22 @@ class Database {
         }
     }
 
+    public function deleteData() {
+        if (isset($_GET["id"])) {
+            $id = htmlspecialchars($_GET["id"]);
+        
+            $sql = "DELETE FROM clients_data WHERE id = $id ";
+            $result = $this->connection->query($sql);
+        
+            if(!$result) {
+                echo "Unable to delete client: " .$this->connection->connect_error;
+            }
+        
+            header("location: index.php");
+            exit;
+        }
+        }
+
     public function getConnection() {
         return $this->connection;
     }
--- delete.php
+++ delete.php
@@ -1,17 +1,5 @@
 <?php
 require_once 'backend/dbconnection.php';
 
-if (isset($_GET["id"])) {
-    $id = htmlspecialchars($_GET["id"]);
-
-    $sql = "DELETE FROM clients_data WHERE id = $id ";
-    $result = $connection->query($sql);
-
-    if(!$result) {
-        echo "Unable to delete client: " .$connection->connect_error;
-    }
-
-    header("location: index.php");
-    exit;
-}
+$deleteData = $connect->deleteData();
 
